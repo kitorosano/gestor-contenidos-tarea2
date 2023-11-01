9f7450b9a77d7d91c334337597d6f6df7bf406b1
Kitorosano <rosanoesteban2@gmail.com>
2023-10-31 23:09:59 -0300

    modifing rrs for build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shiroe-gestor-contenidos-tarea2.netlify.app/',
-  output: 'hybrid',
   integrations: [tailwind()]
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,11 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
-import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-  const posts = await getCollection("posts");
   return rss({
     title: 'Tarea 2 Contenidos | Blog',
     description: 'Blog de la Tarea 2 de Gestor de Contenidos',
     site: context.site,
-    items: posts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      link: `/posts/${post.slug}/`,
-    })),
-    customData: `<language>es</language>`,
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>es</language>`
   });
 }
